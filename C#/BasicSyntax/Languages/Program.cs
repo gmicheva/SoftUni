f1bf750492cab7f83270e55b41a4af887aafd17b
Galina Micheva <galina.i.micheva@gmail.com>
2021-02-08 20:43:41 +0200

    Added C# exercises

---
+++ C#/BasicSyntax/Languages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string language = null;
+
+            switch (country)
+            {
+                case "England":
+                case "Usa":
+                    language = "English";
+                    break;
+                case "Mexico":
+                    language = "Spanish";
+                    break;
+                default:
+                    language = "Unknown";
+                    break;
+            }
+
+            Console.WriteLine(language);
+        }
+    }
+}
---
+++ C#/BasicSyntax/TimeOut/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TimeOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hour++;
+                minutes -= 60;
+            }
+
+            if (hour > 23)
+            {
+                hour = 0;
+            }
+
+            Console.WriteLine($"{hour}:{minutes:D2}");
+        }
+    }
+}
