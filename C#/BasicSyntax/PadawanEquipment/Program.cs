dfecd54497afcc43ca5034495fac1a35313342fa
Galina Micheva <galina.i.micheva@gmail.com>
2021-02-11 13:40:36 +0200

    Added C# exercises

---
+++ C#/BasicSyntax/PadawanEquipment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceLightsabers = double.Parse(Console.ReadLine());
+            double priceRobes = double.Parse(Console.ReadLine());
+            double priceBelts = double.Parse(Console.ReadLine());
+            int freeBelts = students / 6;
+
+            double allPrice = Math.Ceiling(students * 1.1) * priceLightsabers  + 
+                              students * priceRobes + 
+                              (students-freeBelts)*priceBelts;
+
+            if (money >= allPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {allPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(allPrice - money):F2}lv more.");
+            }
+        }
+    }
+}
---
+++ C#/BasicSyntax/RageExpenses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int countTrashesHeadset = lostGames / 2;
+            int countTrashesMouse = lostGames / 3;
+            int countTrashesKeyboard = lostGames / 6;
+            int countTrashesDisplay = countTrashesKeyboard / 2;
+
+            double allExpenses = countTrashesHeadset * headsetPrice +
+                                 countTrashesMouse * mousePrice +
+                                 countTrashesKeyboard * keyboardPrice +
+                                 countTrashesDisplay * displayPrice;
+
+            Console.WriteLine($"Rage expenses: {allExpenses:F2} lv.");
+        }
+    }
+}
