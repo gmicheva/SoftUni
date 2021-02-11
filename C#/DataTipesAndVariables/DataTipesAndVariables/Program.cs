48be24eca6c37692645dd90f79ab4c149ba39e5b
Galina Micheva <galina.i.micheva@gmail.com>
2021-02-11 13:42:33 +0200

    Added C# exercises

---
+++ C#/DataTipesAndVariables/DataTipesAndVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = int.Parse(Console.ReadLine());
+            decimal dollars = pounds * 1.31M;
+
+            Console.WriteLine($"{dollars:F3}");
+        }
+    }
+}
