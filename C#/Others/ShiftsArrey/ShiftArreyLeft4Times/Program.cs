8e7faba85d558a6e5aea6919ceb2b79513b4340e
Galina Micheva <galina.i.micheva@gmail.com>
2021-02-15 15:59:22 +0200

    Added C# Exercise

---
+++ C#/Others/ShiftsArrey/ShiftArreyLeft4Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ShiftArreyLeftByGivenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            int numOfRotated = int.Parse(Console.ReadLine()); //number of rotated
+
+            for (int i = 0; i < numOfRotated; i++)
+            {
+                int firstEl = input[0];
+
+                for (int j = 0; j < input.Length-1; j++)
+                {
+                    input[j] = input[j + 1];                    
+                }
+                input[input.Length - 1] = firstEl;
+            }
+            foreach (var item in input)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
---
+++ C#/Others/ShiftsArrey/ShiftsArrey/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ShiftsArrey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 1, 2, 3, 4, 5 };
+            int ellement0 = arr[0];
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.Length - 1] = ellement0;
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
